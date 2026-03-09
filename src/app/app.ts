15a1e9dd938cc66700f38c193fd97843b98a462b
mosesfrancis-source <moses.francis@ndsu.edu>
2026-03-09 12:43:21 -0500

    Update the HomeComponent template to use @for

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,11 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { Home } from  './home/home';
+import { Home } from './home/home';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Home],
+  imports: [Home],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   title = 'assignment-3-homes-app';
